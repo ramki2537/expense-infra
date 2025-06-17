13068d804982364d3ec9fdf7687b0579d4617317
Rama <ramki2537@gmail.com>
2025-06-17 20:09:55 +0800

    second commit

--- 70-ingress-alb/data.tf
+++ 70-ingress-alb/data.tf
@@ -10,7 +10,6 @@ data "aws_ssm_parameter" "alb_ingress_sg_id" {
   name = "/${var.project_name}/${var.environment}/alb_ingress_sg_id"
 }
 
-
 data "aws_ssm_parameter" "ingress_alb_certificate_arn" {
   name = "/${var.project_name}/${var.environment}/ingress_alb_certificate_arn"
 }
