891be6f12cb6afb4ac140ef7dcfbf940688f0b06
Rama <ramki2537@gmail.com>
2025-06-17 19:35:40 +0800

    second commit

--- 70-ingress-alb/local.tf
+++ 70-ingress-alb/local.tf
@@ -1,7 +1,7 @@
 locals {
   # Stringlist to list
   public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
-  alb_ingress_sg_id = data.aws_ssm_parameter.web_ingress_sg_id.value
+  alb_ingress_sg_id = data.aws_ssm_parameter.alb_ingress_sg_id.value
   ingress_alb_certificate_arn = data.aws_ssm_parameter.ingress_alb_certificate_arn.value
   resource_name = "${var.project_name}-${var.environment}"
   vpc_id = data.aws_ssm_parameter.vpc_id.value
